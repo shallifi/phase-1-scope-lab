5464a731e5ecec1c48102f93e5e4e85eb6cc1983
Benjamin <shallifi@hotmail.com>
2022-03-28 22:26:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "joe"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+console.log(upperCaseCustomerName);
+
+
+function setBestCustomer() {
+    bestCustomer = `not bob`
+}
+//when calling a variable I didn't need to use let or const
+// when inside a function
+
+console.log(bestCustomer);
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "henry"
+}
+
+changeLeastFavoriteCustomer();
+
+//this has err_aborted 404 line 28
+//this has err_aborted 404 line 37
+//
